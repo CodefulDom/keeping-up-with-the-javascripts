86d630ee5b449f2ece137a5673295accce7f0609
codefuldom <dominiquehallan@gmail.com>
2019-03-15 21:02:58 -0500

    :ship: working on functions

--- functions/introToFunctions.js
+++ functions/introToFunctions.js
@@ -9,3 +9,10 @@ const add = (a, b) => a + b
 
 const sum = add(1, 7)
 console.log(sum)
+
+const squared = (num) => {
+  return num * num
+}
+
+const squareRoot = squared(6)
+console.log(typeof squareRoot)
