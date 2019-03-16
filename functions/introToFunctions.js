b49be2f2ebaf4f7cdcea06cc4d73baa967af78e6
codefuldom <dominiquehallan@gmail.com>
2019-03-15 20:43:33 -0500

    :ship: working on functions

---
+++ functions/introToFunctions.js
@@ -0,0 +1,11 @@
+function sayHello(name) {
+  return `Hello, ${name}!`
+}
+
+const hello = sayHello('Dominique Israel Hallan')
+console.log(hello)
+
+const add = (a, b) => a + b
+
+const sum = add(1, 7)
+console.log(sum)
