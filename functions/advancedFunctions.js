0d8bba7b7e0055dd96a978a3a08fb8acbae5a980
codefuldom <dominiquehallan@gmail.com>
2019-03-16 22:33:19 -0500

    :ship: working on advanced function

--- functions/advancedFunctions.js
+++ functions/advancedFunctions.js
@@ -40,3 +40,11 @@ console.log(multiplier(2, 2))
 
 const squared = (num) => num * num
 console.log(squared(2))
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const filteredNumbers = numbers.filter((num) => {
+  return num % 2 === 0
+})
+
+console.log(filteredNumbers)
