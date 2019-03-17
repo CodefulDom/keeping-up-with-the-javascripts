a456e4e734c3ea4379d26c8f30d9c33dd52e2051
codefuldom <dominiquehallan@gmail.com>
2019-03-16 21:28:29 -0500

    :ship: working on closure

--- functions/nestedFunctions.js
+++ functions/nestedFunctions.js
@@ -8,3 +8,18 @@ function squareAndMultiply(num1, num2) {
 }
 
 console.log(squareAndMultiply(2, 4))
+
+function addAndMultiply(num1, num2) {
+  function add(x) {
+    return x + x
+  }
+  function multiply(y) {
+    return y * y
+  }
+
+  return add(num1) + multiply(num2)
+}
+
+console.log(addAndMultiply(3, 3))
+
+console.log(3 + 3 + 3 * 3)
