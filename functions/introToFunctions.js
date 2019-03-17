e375d279dfaf23685e3a6ebb65ba4b742f0f8136
codefuldom <dominiquehallan@gmail.com>
2019-03-16 21:07:15 -0500

    :ship: working on functions

--- functions/introToFunctions.js
+++ functions/introToFunctions.js
@@ -30,3 +30,33 @@ const numberAdder = function(num1, num2) {
 }
 
 console.log(numberAdder(squareNumber(2), 1))
+
+// Object - Passed by value
+
+let myNumber = 100
+
+function addTwenty(num) {
+  return num + 20
+}
+
+console.log(addTwenty(myNumber))
+
+const myInfo = { name: 'Chris', age: 30 }
+
+function changeAge(myObj) {
+  myObj.age = 100
+}
+
+console.log(myInfo)
+changeAge(myInfo)
+console.log(myInfo)
+
+const myNumbers = [1, 2, 3, 4, 5]
+
+function addToArray(myArr) {
+  myArr.push(6)
+}
+
+console.log(myNumbers)
+addToArray(myNumbers)
+console.log(myNumbers)
