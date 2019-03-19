6f0ac483abe0de50c879ca6c1d02d552f77e4bf7
codefuldom <dominiquehallan@gmail.com>
2019-03-19 00:34:45 -0500

    :ship: working on switch statements

--- switch-statements/switchStatement.js
+++ switch-statements/switchStatement.js
@@ -2,21 +2,27 @@
 
 /*
 
-	switch(expression) {
-		case vaule1:
-			statement to be executed when result of expression matches value1
-			break;
-		case value2:
-			...
-			break
-	}
+  switch(expression) {
+    case vaule1:
+      statement to be executed when result of expression matches value1
+      break;
+    case value2:
+      ...
+      break
+  }
 */
 
-const myFruit = 'apple'
-if (myFruit === 'apple') {
-  console.log('awesome, I love apples!')
-} else if (myFruit === 'oranges') {
-  console.log('I like oranges')
-} else if (myFruit === 'pears') {
-  console.log('I like pears')
+const myFruit = 'dkdkkd'
+switch (myFruit) {
+  case 'apple':
+    console.log(`Great, I love apples!`)
+    break
+  case 'orange':
+    console.log('Oranges are awesome')
+    break
+  case 'banana':
+    console.log('Banana is my least favorite')
+  default:
+    console.log('huh?? 👨🏾‍💻')
+    break
 }
